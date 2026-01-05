48b7b890c279c4f3f59106ba3512cbd71c4096e3
Ibrahim <ibrahimmohamedabdelsadek@gmail.com>
2026-01-05 06:57:49 +0200

    Add that client can send masseges to all client and cover race condition with locks mutex from async

--- main.go
+++ main.go
@@ -3,11 +3,43 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
+var (
+	clients = make([]net.Conn,0)
+	mutex = sync.Mutex{}
+)
+func addClient(conn net.Conn) {
+    mutex.Lock()
+    clients = append(clients, conn)
+    mutex.Unlock()
+}
+func removeClient(conn net.Conn) {
+    mutex.Lock()
+	// it gives 2 things 1 is index i from 0 to clints number and conn itslf its the connection itsself maybe connA connB 
+     for i, c := range clients{
+		if c == conn {
+			clients = append(clients[:i], clients[i+1:]...)
+			break
+		}
+	}
+    mutex.Unlock()
+}
+func broadcast(message string, sender net.Conn){
+	mutex.Lock()
+	for _,client := range clients{
+		if client != sender{
+			client.Write([]byte(message))
+		}
+	}
+	mutex.Unlock()
+}
+
 func handleConnection(conn net.Conn){
 	defer conn.Close()
+	defer removeClient(conn)
 	fmt.Println("Client connected:", conn.RemoteAddr())
-
+addClient(conn)
 	//read
 buffer := make([]byte,1024)
 	for {
@@ -20,8 +52,7 @@ buffer := make([]byte,1024)
 	message := string(buffer[:n])
 
 	fmt.Printf("Received: %s", message)
-	conn.Write([]byte("Server Respone: " + message))
-	}
+    broadcast(message, conn)	}
 }
 
 func main(){
